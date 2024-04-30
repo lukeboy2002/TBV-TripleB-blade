d158d1406ee55b3afa0ea6324387c262d6a281bf
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2024-04-30 21:14:25 +0200

    auth to username

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,14 +27,15 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->unique()->username(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
-            'profile_photo_path' => null,
+//            'profile_photo_path' => null,
+            'profile_photo_path' => fake()->imageUrl(360, 360, 'animals', true, 'dogs', true),
             'current_team_id' => null,
         ];
     }
